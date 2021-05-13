a3f18e6cfb968093b04005fdc737f5a47ce0d1c1
Michael Bergerson <michealbergerson@gmail.com>
2021-05-12 23:16:37 -0700

    bug testing v0.21

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -71,9 +71,9 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         }),
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
-            httpOnly: true,
+            httpOnly: false,
             sameSite: "lax",
-            secure: constants_1.__prod__,
+            secure: true,
         },
         saveUninitialized: false,
         secret: process.env.SESSION_SECRET,
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -72,9 +72,9 @@ const main = async () => {
         }),
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
-            httpOnly: true,
+            httpOnly: false,
             sameSite: "lax",
-            secure: __prod__,
+            secure: true,
         },
         saveUninitialized: false,
         secret: process.env.SESSION_SECRET,
